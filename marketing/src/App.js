53d3e243f877452428a293cd57ac4cbdd685bb87
GururajLatukar <gururaj.latukar@gmail.com>
2022-07-26 20:19:21 +0530

    generate random classname for each service in production

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = new createGenerateClassName({
+    productionPrefix: 'ma'
+});
+
 export default () => {
     return <div>
-        <StylesProvider >
+        <StylesProvider generateClassName={generateClassName} >
             <BrowserRouter>
                 <Routes>
                     <Route exact path="/pricing" element={<Pricing />} />
